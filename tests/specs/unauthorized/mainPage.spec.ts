5badb2771e00d408a7a5fa353367d3f3e46665dc
aleks2392 <alekhin.aleks92@gmail.com>
2025-04-28 11:19:10 +0200

    test

--- tests/specs/unauthorized/mainPage.spec.ts
+++ tests/specs/unauthorized/mainPage.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '../../fixtures/fixtures';
+import { test } from '../../fixtures/fixtures';
 
 test('Checking availability elments of header', async ({ mainPage }) => {
   await mainPage.headerHasCorrectAria();
